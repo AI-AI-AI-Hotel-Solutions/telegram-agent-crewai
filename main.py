9fc09bb859634d54d43ad219d72b847ca0e8f87d
DennisSanntos <sined.marecas@gmail.com>
2025-06-27 04:49:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 import requests
+import traceback
 from crew_config import process_message
 
 TOKEN = '7504265835:AAGkAEHaMmBW59SlfQ0ga9XuUF-lsx83zRU'
@@ -10,9 +11,6 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     return 'Bot online!', 200
-
-import traceback
-...
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -28,6 +26,6 @@ def webhook():
     requests.post(TELEGRAM_URL, json={'chat_id': chat_id, 'text': reply})
     return '', 200
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
+
