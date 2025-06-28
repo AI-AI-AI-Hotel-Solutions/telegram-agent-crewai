88c7597a19f1d05f0cf6a240cf12adab2c14a718
DennisSanntos <sined.marecas@gmail.com>
2025-06-28 02:57:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 import requests
 import traceback
 from crew_config import process_message
+from sheet_handler import executar_acao
 
 TOKEN = '7504265835:AAGkAEHaMmBW59SlfQ0ga9XuUF-lsx83zRU'
 TELEGRAM_URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
